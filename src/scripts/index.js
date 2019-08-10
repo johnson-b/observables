db457c3e63d4698ef0ef87a3839c556a5219f64f
Bryan <johnson.4652@gmail.com>
2019-08-10 16:04:18 -0400

    initial commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,33 @@
+import { fromEvent, interval } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
+
 import '../styles/index.scss';
 
-console.log('webpack starterkit');
+(function() {
+  class Timer {
+    
+    constructor() {
+      this.startButton = document.querySelector('#start-button');
+      this.stopButton = document.querySelector('#stop-button');
+      this.timerContainer = document.querySelector('.timer-container');
+
+      this.start$ = fromEvent(this.startButton, 'click');
+      this.stop$ = fromEvent(this.stopButton, 'click');
+
+      this.ticks$ = interval(100);
+      this.init();
+    }
+    
+    init() {
+      this.start$.subscribe(() => {
+        this.ticks$.pipe(
+          map((val) => (val / 10).toFixed(1)),
+          takeUntil(this.stop$)
+        ).subscribe((num) => this.timerContainer.innerText = num); 
+      });
+    }
+
+  }
+
+  new Timer();
+})();
