c471feaab73c5c5e37436e4284212e3f7758e676
Bryan <johnson.4652@gmail.com>
2019-08-13 08:31:20 -0400

    random color for each circle WIP

--- observables-playground/src/app/app.module.ts
+++ observables-playground/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,6 +8,7 @@ import { StopWatchComponent } from './stop-watch/stop-watch.component';
 import { HomeComponent } from './home/home.component';
 import { TypeAheadWipComponent } from './type-ahead-wip/type-ahead-wip.component';
 import { TypeAheadComponent } from './type-ahead/type-ahead.component';
+import { TypeAheadVisualComponent } from './type-ahead-visual/type-ahead-visual.component';
 
 @NgModule({
   declarations: [
@@ -14,10 +16,12 @@ import { TypeAheadComponent } from './type-ahead/type-ahead.component';
     StopWatchComponent,
     HomeComponent,
     TypeAheadWipComponent,
-    TypeAheadComponent
+    TypeAheadComponent,
+    TypeAheadVisualComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
--- observables-playground/src/app/type-ahead/type-ahead.component.ts
+++ observables-playground/src/app/type-ahead/type-ahead.component.ts
@@ -1,7 +1,8 @@
 import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { fromEvent, of } from 'rxjs';
-import { map, filter, switchMap, mergeMap, debounceTime, distinctUntilChanged, catchError } from 'rxjs/operators';
+import { map, filter, switchMap, mergeMap, debounceTime, distinctUntilChanged, catchError, tap } from 'rxjs/operators';
 import { ajax } from 'rxjs/ajax';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-type-ahead',
@@ -16,28 +17,52 @@ export class TypeAheadComponent implements AfterViewInit {
   @ViewChild('questionsContainer')
   questionContainer: ElementRef;
 
+  keyUps: any[] = [];
+  filters: any[] = [];
+  debounces: any[] = [];
+  distincts: any[] = [];
+  results: any[] = [];
+  questions: any[] = [];
+
   // tslint:disable-next-line: max-line-length
   private slackApi = (searchText) => `https://api.stackexchange.com/2.2/search?key=U4DMV*8nvpm3EOpvf69Rxw((&site=stackoverflow&order=desc&sort=activity&intitle=${searchText}&filter=default`;
 
-  constructor() { }
+  constructor(private sanatizer: DomSanitizer) { }
 
   ngAfterViewInit() {
     fromEvent(this.searchInput.nativeElement, 'keyup').pipe(
       map((event: any) => event.target.value),
+      tap((value) => this.keyUps.push(this.getCircle())),
       filter((searchText) => !!searchText && searchText.length > 3),
+      tap((value) => this.filters.push(this.getCircle())),
       debounceTime(333),
+      tap((value) => this.debounces.push(this.getCircle())),
       distinctUntilChanged(),
+      tap((value) => this.distincts.push(this.getCircle())),
       switchMap((searchText) => {
         this.questionContainer.nativeElement.innerHTML = '';
         return ajax(this.slackApi(searchText));
       }),
-      catchError(err => of({ response: { items: []}})),
-      mergeMap((response) => response.response.items),
-    ).subscribe(
-      (question: any) => this.questionContainer.nativeElement.innerHTML += '<br/>' + question.title,
+      tap((results) => this.results.push(this.getCircle())),
+      catchError(err => {
+        console.error('Error', err);
+        return of({ response: { items: []}});
+      }),
+      mergeMap((response) => response.response.items)
+    ).subscribe((question: any) => {
+        this.questionContainer.nativeElement.innerHTML += '<br/>' + question.title;
+        this.questions.push(this.getCircle());
+      },
       err => alert(err.message),
       () => console.log('done')
     );
   }
 
+  getCircle() {
+    return this.sanatizer.bypassSecurityTrustHtml(`<div class="circle" style="background-color: ${this.getRandomColor()}"></div>`);
+  }
+
+  getRandomColor() {
+    return `#${Math.random().toString(16).substr(-6)}`;
+  }
 }
